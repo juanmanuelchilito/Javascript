7ae58b848a8ebce53311b1b292aa6391e936bd2c
juanmanuelchilito <85502291+juanmanuelchilito@users.noreply.github.com>
2021-10-25 22:06:59 -0500

    Add files via upload

--- app.js
+++ app.js
@@ -1,3 +1,19 @@
-let url = require('./prueba')
+const mascota = require("./mascota")
+function esperar(ms) {
+  let d = new Date();
+  let d2 = null;
+  do { d2 = new Date(); }
+  while (d2 - d < ms);
+}
 
-console.log(url);
+mascota.checkeo();
+while (mascota.sigueConVida()) {
+  console.log(mascota.status());
+  mascota.vivir();
+  mascota.comer();
+  mascota.pasear();
+  mascota.checkeo();
+  esperar(600);
+}
+
+console.log(!mascota.sigueConVida() ? "R.I.P. lo siento..." : "");
